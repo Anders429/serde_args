0ca4ea82a23bff92bce5bd90efc9101d782e420c
Anders429 <anders.stefan.evensen@gmail.com>
2024-08-24 17:02:13 -0600

    Remove unused TryFrom implementation.

--- macros/src/container.rs
+++ macros/src/container.rs
@@ -1,4 +1,4 @@
-use proc_macro2::{Span, TokenStream};
+use proc_macro2::Span;
 use syn::{
     parse,
     parse::{Parse, ParseStream},
@@ -8,22 +8,6 @@ use syn::{
 pub(crate) enum Container {
     Struct(ItemStruct),
     Enum(ItemEnum),
-}
-
-impl TryFrom<Item> for Container {
-    type Error = TokenStream;
-
-    fn try_from(item: Item) -> Result<Self, Self::Error> {
-        match item {
-            Item::Struct(r#struct) => Ok(Self::Struct(r#struct)),
-            Item::Enum(r#enum) => Ok(Self::Enum(r#enum)),
-            item => Err(parse::Error::new(
-                Span::call_site(),
-                format!("cannot use `serde_args::help` macro on {:?} item", item),
-            )
-            .into_compile_error()),
-        }
-    }
 }
 
 impl Parse for Container {
