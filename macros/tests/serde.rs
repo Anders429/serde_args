08ccc307a138aca2a3603f8fa5d67e333571247a
Anders429 <anders.stefan.evensen@gmail.com>
2024-08-30 09:38:37 -0600

    Test deserialization behavior with help generation.

---
+++ macros/tests/serde.rs
@@ -0,0 +1,103 @@
+//! Testing serialization and deserialization behavior when using the `#[help]` attribute.
+
+use claims::assert_ok_eq;
+use serde::Deserialize;
+use serde_args_macros::help;
+use serde_assert::{Deserializer, Token};
+
+#[help]
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+struct Struct {
+    foo: u32,
+    bar: String,
+}
+
+#[test]
+fn struct_deserialize() {
+    let tokens = [
+        Token::NewtypeStruct { name: "Struct" },
+        Token::Struct {
+            name: "Struct",
+            len: 2,
+        },
+        Token::Field("foo"),
+        Token::U32(42),
+        Token::Field("bar"),
+        Token::Str("baz".into()),
+        Token::StructEnd,
+    ];
+    let mut deserializer = Deserializer::builder(tokens).build();
+    assert_ok_eq!(
+        Struct::deserialize(&mut deserializer),
+        Struct {
+            foo: 42,
+            bar: "baz".into(),
+        }
+    );
+}
+
+#[help]
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+enum Enum {
+    Unit,
+    Newtype(u32),
+    Struct { foo: u32, bar: String },
+}
+
+#[test]
+fn enum_unit_deserialize() {
+    let tokens = [
+        Token::NewtypeStruct { name: "Enum" },
+        Token::UnitVariant {
+            name: "Enum",
+            variant_index: 0,
+            variant: "Unit",
+        },
+        Token::StructEnd,
+    ];
+    let mut deserializer = Deserializer::builder(tokens).build();
+    assert_ok_eq!(Enum::deserialize(&mut deserializer), Enum::Unit,);
+}
+
+#[test]
+fn enum_newtype_deserialize() {
+    let tokens = [
+        Token::NewtypeStruct { name: "Enum" },
+        Token::NewtypeVariant {
+            name: "Enum",
+            variant_index: 1,
+            variant: "Newtype",
+        },
+        Token::U32(42),
+        Token::StructEnd,
+    ];
+    let mut deserializer = Deserializer::builder(tokens).build();
+    assert_ok_eq!(Enum::deserialize(&mut deserializer), Enum::Newtype(42),);
+}
+
+#[test]
+fn enum_struct_deserialize() {
+    let tokens = [
+        Token::NewtypeStruct { name: "Enum" },
+        Token::StructVariant {
+            name: "Enum",
+            variant_index: 2,
+            variant: "Struct",
+            len: 2,
+        },
+        Token::Field("foo"),
+        Token::U32(42),
+        Token::Field("bar"),
+        Token::Str("baz".into()),
+        Token::StructVariantEnd,
+        Token::StructEnd,
+    ];
+    let mut deserializer = Deserializer::builder(tokens).build();
+    assert_ok_eq!(
+        Enum::deserialize(&mut deserializer),
+        Enum::Struct {
+            foo: 42,
+            bar: "baz".into(),
+        },
+    );
+}
