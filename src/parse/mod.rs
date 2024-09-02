dfd4c0c602be9195abf9c4c2561107552416fdc2
Anders429 <anders.stefan.evensen@gmail.com>
2024-09-02 09:54:09 -0600

    Collapse if-else statement when checking for remaining arguments.

--- src/parse/mod.rs
+++ src/parse/mod.rs
@@ -95,30 +95,28 @@ where
             } else {
                 break;
             }
-        } else {
-            if let Some(token) = parsed_args.next_token() {
-                match token {
-                    Token::Positional(value) => {
-                        return Err(Error::UnexpectedArgument(value));
-                    }
-                    Token::Optional(value) => {
-                        return Err(Error::UnrecognizedOption {
-                            name: String::from_utf8_lossy(&value).into(),
-                            expecting: vec!["help", "h"]
-                                .into_iter()
-                                .chain(shape.trailing_options().into_iter().flat_map(|field| {
-                                    iter::once(field.name).chain(field.aliases.iter().copied())
-                                }))
-                                .collect(),
-                        });
-                    }
-                    Token::EndOfOptions => {
-                        end_of_options = true;
-                    }
+        } else if let Some(token) = parsed_args.next_token() {
+            match token {
+                Token::Positional(value) => {
+                    return Err(Error::UnexpectedArgument(value));
                 }
-            } else {
-                break;
+                Token::Optional(value) => {
+                    return Err(Error::UnrecognizedOption {
+                        name: String::from_utf8_lossy(&value).into(),
+                        expecting: vec!["help", "h"]
+                            .into_iter()
+                            .chain(shape.trailing_options().into_iter().flat_map(|field| {
+                                iter::once(field.name).chain(field.aliases.iter().copied())
+                            }))
+                            .collect(),
+                    });
+                }
+                Token::EndOfOptions => {
+                    end_of_options = true;
+                }
             }
+        } else {
+            break;
         }
     }
 
