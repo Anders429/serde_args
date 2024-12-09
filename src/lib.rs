a2eeb505d8dccb71583a61ba73f57c83d43ec541
Anders429 <anders.stefan.evensen@gmail.com>
2024-12-09 16:23:26 -0700

    Root-level documentation and format specification.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,244 @@
+//! Command line argument parsing with [`serde`].
+//!
+//! This library allows parsing command line arguments into types implementing [`Deserialize`].
+//! Included are all of the typical features you'd find in an command line argument parsing
+//! library, including help generation, help customization, version support, and flexible parsing
+//! options.
+//!
+//! Unlike other argument parsing libraries, `serde_args` uses `serde`'s traits to define the
+//! command line interface. No builder or derive interface is provided; instead, any object
+//! implementing `Deserialize` can be used. This means that you can use `serde`'s own derive macros
+//! (that you are likely already familiar with) or implement the `Deserialize` trait by hand to
+//! define your command line interface.
+//!
+//! `serde_args` defines an unambiguous deserialization format through internal [`Deserializer`]s;
+//! therefore it should be noted that not every command line interface can be represented using it.
+//! Notably, optional positional parameters are not supported at all, nor are default command
+//! values. The format defined here requires that all positional arguments (those *without* a `-`
+//! or `--` preceeding them) be required arguments, and that all other arguments be preceeded with
+//! either a `-` or `--` (including compound types). See the [format specification](specification)
+//! for more details.
+//!
+//! # Parsing Arguments
+//!
+//! To use `serde_args` for your own command line argument parsing, you must first define a type
+//! implementing `serde`'s [`Deserialize`] trait. This can be done using `serde`'s derive macro or
+//! by implementing the trait by hand. For example, a simple program could be defined as:
+//!
+//! ``` rust
+//! use serde::Deserialize;
+//! # use serde_derive::Deserialize;
+//! use std::path::PathBuf;
+//!
+//! #[derive(Deserialize)]
+//! #[serde(expecting = "An example program")]
+//! struct Args {
+//!     path: PathBuf,
+//!     #[serde(alias = "f")]
+//!     force: bool,
+//! }
+//!
+//! fn main() {
+//!     let args: Args = match serde_args::from_args() {
+//!         Ok(args) => args,
+//!         Err(error) => {
+//!             println!("{error}");
+//!             return;
+//!         }
+//!     };
+//!     // Execute your program with `args`...
+//! }
+//! ```
+//!
+//! Command-based interfaces can be defined using `enums`:
+//!
+//! ``` rust
+//! use serde::Deserialize;
+//! # use serde_derive::Deserialize;
+//! use std::path::PathBuf;
+//!
+//! #[derive(Deserialize)]
+//! #[serde(expecting = "A command-based interface")]
+//! #[serde(rename_all = "kebab-case")]
+//! enum Command {
+//!     Add {
+//!         path: PathBuf,
+//!     },
+//!     Commit {
+//!         #[serde(alias = "m")]
+//!         message: Option<String>,
+//!     },
+//!     Push {
+//!         #[serde(alias = "f")]
+//!         force: bool,
+//!     },
+//! }
+//!
+//! fn main() {
+//!     let command: Command = match serde_args::from_args() {
+//!         Ok(command) => command,
+//!         Err(error) => {
+//!             println!("{error}");
+//!             return;
+//!         }
+//!     };
+//!     // Execute your program with `command`...
+//! }
+//! ```
+//!
+//! For simple use cases you can also use existing types that
+//! already implement `Deserialize`:
+//!
+//! ``` rust
+//! fn main() {
+//!     let value: String = match serde_args::from_args() {
+//!         Ok(value) => value,
+//!         Err(error) => {
+//!             println!("{error}");
+//!             return;
+//!         }
+//!     };
+//!     // Execute your program with `value`...
+//! }
+//! ```
+//!
+//! Note that the only way to deserialize using this crate is through [`from_args()`] and
+//! [`from_args_seed()`]. No public [`Deserializer`] is provided.
+//!
+//! # Error Formatting
+//!
+//! On failure, [`from_args()`] will return an [`Error`]. This will occur when the provided type is
+//! incompatible with this crate (see [Supported `serde` Attributes](#supported-serde-attributes)
+//! for common reasons why types are not compatible), when the user has input command line
+//! arguments that cannot be parsed into the provided type, or when the user requests the generated
+//! help message (either through the `--help` flag or by providing no arguments). In any case, the
+//! returned `Error` implements the [`Display`] trait and is able to be printed and displayed to
+//! the user.
+//!
+//! For example, a program taking a single unsigned integer value as a parameter would print an
+//! error that occurred like so:
+//!
+//! ```rust
+//! if let Err(error) = serde_args::from_args::<usize>() {
+//!     println!("{error}");
+//! }
+//! ```
+//!
+//! To print an error that is formatted with ANSI color sequences, use the "alternate" form of
+//! printing with the `#` flag:
+//!
+//! ```rust
+//! if let Err(error) = serde_args::from_args::<usize>() {
+//!     println!("{error:#}");
+//! }
+//! ```
+//!
+//! # Customization
+//!
+//! `serde_args` allows for customizing in the form of messages to be displayed in help output and
+//! the displaying of version information. These are most easily customized using the
+//! [`#[generate]`](generate) attribute (requires the `macros` feature`). This macro must be
+//! combined with `serde`'s `Deserialize` derive macro.
+//!
+//! ## Custom Help Messages
+//!
+//! Descriptions for each of your fields or variants can be automatically imported from your struct
+//! or enum's doc comment using `#[generate]` with `doc_help` as a parameter:
+//!
+//! ``` rust
+//! use serde::Deserialize;
+//! # use serde_derive::Deserialize;
+//! use std::path::PathBuf;
+//!
+//! /// An example program.
+//! #[derive(Deserialize)]
+//! #[serde_args::generate(doc_help)]
+//! struct Args {
+//!     /// The path to operate on.
+//!     path: PathBuf,
+//!     /// Whether the program's behavior should be forced.
+//!     #[serde(alias = "f")]
+//!     force: bool,
+//! }
+//!
+//! fn main() {
+//!     let args: Args = match serde_args::from_args() {
+//!         Ok(args) => args,
+//!         Err(error) => {
+//!             println!("{error}");
+//!             return;
+//!         }
+//!     };
+//!     // Execute your program with `args`...
+//! }
+//! ```
+//!
+//! ## Version Information
+//!
+//! To automatically make the version of your crate available through a `--version` flag, use
+//! `#[generate]` with `version` as a parameter:
+//!
+//! ```rust
+//! use serde::Deserialize;
+//! # use serde_derive::Deserialize;
+//!
+//! #[derive(Deserialize)]
+//! #[serde_args::generate(version)]
+//! struct Args {
+//!     foo: String,
+//!     bar: bool,
+//! }
+//!
+//! fn main() {
+//!     let args: Args = match serde_args::from_args() {
+//!         Ok(args) => args,
+//!         Err(error) => {
+//!             println!("{error}");
+//!             return;
+//!         }
+//!     };
+//!     // Execute your program with `args`...
+//! }
+//! ```
+//!
+//! ## Customization Without Deriving
+//!
+//! To provide these customization options without deriving, see
+//! [`expecting()` Option Specification](specification/index.html#expecting-option-specification).
+//!
+//! # Supported `serde` Attributes
+//!
+//! Nearly all `serde` attributes are supported. Those that are not supported are those that
+//! require a self-describing deserializer (the format defined by `serde_args` is **not**
+//! self-describing). Specifically, the following attributes will not work:
+//!
+//! - [`#[serde(flatten)]`](https://serde.rs/field-attrs.html#flatten)
+//! - [`#[serde(tag = "type")]`](https://serde.rs/container-attrs.html#tag) - Doesn't work for
+//! enums, but it will work for structs.
+//! - [`#[serde(tag = "t", content = "c")]`](https://serde.rs/container-attrs.html#tag--content)
+//! - [`#[serde(untagged)]`](https://serde.rs/container-attrs.html#untagged) - Not allowed on enums
+//! or on variants.
+//! - [`#[serde(other)]`](https://serde.rs/variant-attrs.html#other)
+//!
+//! Aside from the above list, all other attributes are supported. Some attributes are especially
+//! useful for defining command line interfaces, including:
+//!
+//! - [`#[serde(alias)]`](https://serde.rs/field-attrs.html#alias) - Useful for defining multiple
+//! names for optional fields or command variants.
+//! - [`#[serde(expecting)]`](https://serde.rs/container-attrs.html#expecting) - Can be used to
+//! define a description for your program. Whatever is provided here will be output at the top of
+//! the generated help message.
+//!   - Note that many users will want to use [`#[serde_args::generate(doc_help)]`](generate) to
+//!     automatically populate this message from the container's doc comment instead.
+//! - [`#[serde(rename_all)]`](https://serde.rs/container-attrs.html#rename_all) - Useful for
+//! renaming all field names or enum variants to kebab-case, which is common for command-line
+//! tools.
+//!
+//! [`Deserializer`]: serde::Deserializer
+//! [`Display`]: std::fmt::Display
+
+pub mod specification;
+
 mod de;
 mod error;
 mod key;
---
+++ src/specification.rs
@@ -0,0 +1,267 @@
+//! A full specification of the format defined by `serde_args`.
+//!
+//! Command line interfaces created with `serde_args` are entirely defined by the
+//! [`serde` data types](https://serde.rs/data-model.html#types) requested by the given type's
+//! [`Deserialize`] implementation. The allowed inputs will depend entirely on the methods called
+//! on the deserializer.
+//!
+//! # Primitives
+//!
+//! Primitives will, in all cases besides units, be parsed from the immediate next non-optional
+//! argument string.
+//!
+//! Note that the rules defined here do not all apply to struct fields. Specifically, booleans are
+//! treated differently if called within a struct.
+//!
+//! ## Units
+//!
+//! Units, requested with [`Deserializer::deserialize_unit()`],
+//! [`Deserializer::deserialize_unit_struct()`], or [`VariantAccess::unit_variant()`], are
+//! represented as nothing. No input strings will be parsed.
+//!
+//! ## Booleans
+//!
+//! Booleans, requested with [`Deserializer::deserialize_bool()`], can parse either `true` or
+//! `false`. Note that these values must be lowercase; `TRUE` or `FALSE` are not accepted, nor are
+//! `t`, `f`, `0`, or `1`.
+//!
+//! ## Numeric Values
+//!
+//! Numeric values, meaning integers (signed or unsigned) and floats, will be parsed using the
+//! corresponding numeric type's [`FromStr`] implementation. For example, calling
+//! [`Deserializer::deserialize_u32()`] will result in the next value being parsed with
+//! `u32::from_str()`.
+//!
+//! ## Characters, Strings, and Bytes
+//!
+//! Characters, strings, and bytes will be parsed by interpreting the next value as the given type.
+//! Characters must be exactly one character and strings must be UTF-8. Bytes can be any set of
+//! bytes, although there may be limitations regarding what bytes can actually be passed on the
+//! command line depending on the operating system being used.
+//!
+//! # Optionals
+//!
+//! Optional values, requested with [`Deserializer::deserialize_option()`], will optionally parse
+//! the next value *if* it is an optional value. An optional value is preceeded with `--`. If the
+//! next value is not optional (or we are at the end of the argument list), the returned value is
+//! `None`.
+//!
+//! For example, parsing an `Option<String>` would interpret the argument `--foo` as `Some("foo")`.
+//!
+//! # Structs
+//!
+//! ## Normal Structs
+//!
+//! Structs have three different types of fields: boolean fields, optional fields, and required
+//! fields. The rules for parsing boolean fields and optional fields differ from parsing booleans
+//! and optionals as explained above.
+//!
+//! All boolean and optional fields can be provided at any point and in any order during the
+//! parsing of the struct. This means they can be provided at any point between the required fields
+//! of the struct, immediately before the first field of the struct, or immediately after the last
+//! field of the struct.
+//!
+//! ### Boolean fields
+//! Boolean fields are set to true using the name of the field as an optional value. If the name is
+//! never provided, a default value of `false` is used.
+//!
+//! For example, a boolean field named `foo` could be set to true with the flag `--foo`.
+//!
+//! ### Optional fields
+//!
+//! Optional fields are set using the name of the field as an optional value, followed by the value
+//! itself. If the name is never provided, a default value of `None` is used.
+//!
+//! For example, an optional field named `foo` containing a `String` value could be set using
+//! `--foo bar`. This would set the field's value to `Some("bar")`.
+//!
+//! ### Required fields
+//!
+//! Required fields (sometimes called "positional fields") are all fields that are not booleans or
+//! optionals. They must be provided in the order they are defined and cannot be omitted.
+//!
+//! ## Unit Structs
+//!
+//! See (Units)[#units].
+//!
+//! ## Newtype Structs
+//!
+//! Newtype structs are treated as their contained value. `serde_args` does no special wrapping of
+//! newtype structs.
+//!
+//! Note that options provided in `expecting()` on the newtype struct will overwrite the contained
+//! type's options. See [`expecting()` Option Specification](#expecting-option-specification) for
+//! more details.
+//!
+//! ## Tuple Structs
+//!
+//! Tuple structs are not currently supported.
+//!
+//! # Enums
+//!
+//! Enums (often called "commands" in terms of command line interfaces) are specified by the
+//! identifier parsed from the next available argument (parsed as a string, not as an integer or
+//! any other value). The matched variant type will determine how the next arguments are parsed.
+//!
+//! ## Unit Variants
+//!
+//! See (Units)[#units].
+//!
+//! ## Struct Variants
+//!
+//! The next values will be parsed as the provided struct. See (Structs)[#structs] for more
+//! details.
+//!
+//! ## Newtype Variants
+//!
+//! The next values will be parsed as the contained value. See (Newtype Structs)[#newtype-structs]
+//! for more details.
+//!
+//! ## Tuple Variants
+//!
+//! Tuple variants are not currently supported.
+//!
+//! # Tuples
+//!
+//! Tuples are not currently supported.
+//!
+//! # Sequences
+//!
+//! Sequences are not currently supported.
+//!
+//! # Maps
+//!
+//! Maps are not currently supported.
+//!
+//! # `expecting()` Option Specification
+//!
+//! While most users will likely want to create types using `serde`'s derive macros, some users may
+//! wish to define their [`Deserialize`] implementations by hand. These users will therefore not be
+//! able to use the [`#[generate]`](crate::generate) macro to customize the behavior of the
+//! deserializer and will need to do so by hand. To do so, the [`Visitor::expecting()`]
+//! function in the `Deserialize` implementation must provide that information directly.
+//!
+//! Note that the following techniques can be combined together to specify multiple customizations
+//! on the same container.
+//!
+//! ## Help Messages
+//!
+//! There are two types of custom messages that can be provided: container-level messages and
+//! field/variant-level messages. These can be defined within the same visitor to allow displaying
+//! all kinds of help messages on the same container.
+//!
+//! ### Container-level messages
+//!
+//! For `structs` and `enum`s, the container-level message can be provided by simply writing the
+//! message to the provided formatter.
+//!
+//! ```rust
+//! use serde::de::Visitor;
+//! use std::{
+//!     fmt,
+//!     fmt::Formatter,
+//! };
+//!
+//! struct ContainerVisitor;
+//!
+//! impl<'de> Visitor<'de> for ContainerVisitor {
+//!     type Value = ();
+//!
+//!     fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
+//!         formatter.write_str("My custom message")
+//!     }
+//! }
+//! ```
+//!
+//! ### Field and variant messages
+//!
+//! Custom messages are provided for fields and variants by writing the corresponding message using
+//! the field/variant index provided. The field/variant index is provided through
+//! `formatter.width()`. Write the corresponding message (and nothing else) when a width is
+//! provided corresponding to the field or variant.
+//!
+//! For example, for a struct defined as follows:
+//!
+//! ```rust
+//! struct Container {
+//!     first_field: String,
+//!     second_field: Option<usize>,
+//! }
+//! ```
+//!
+//! One could specify field messages as follows:
+//!
+//! ```rust
+//! use serde::de::Visitor;
+//! use std::{
+//!     fmt,
+//!     fmt::Formatter,
+//! };
+//!
+//! struct ContainerVisitor;
+//!
+//! impl<'de> Visitor<'de> for ContainerVisitor {
+//!     type Value = ();
+//!
+//!     fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
+//!         match formatter.width() {
+//!             Some(0) => formatter.write_str("First field's message"),
+//!             Some(1) => formatter.write_str("Second field's message"),
+//!             _ => formatter.write_str("Container's message"),
+//!         }
+//!     }
+//! }
+//! ```
+//!
+//! ## Version Information
+//!
+//! To specify that a `--version` flag should be used, `expecting()` should provide a version to be
+//! used as version output when `formatter.fill()` is `'v'`. For example:
+//!
+//! ```rust
+//! use serde::de::Visitor;
+//! use std::{
+//!     fmt,
+//!     fmt::Formatter,
+//! };
+//!
+//! struct VersionVisitor;
+//!
+//! impl<'de> Visitor<'de> for VersionVisitor {
+//!     type Value = ();
+//!
+//!     fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
+//!         if formatter.fill() == 'v' {
+//!             formatter.write_str("0.1.0")?;
+//!         }
+//!         Ok(())
+//!     }
+//! }
+//! ```
+//!
+//! Note that the version provided to the formatter must be different than what is provided when
+//! the version is not requested; otherwise it will be ignored and no `--format` flag will be used.
+//!
+//! # Unsupported Deserialization Behavior
+//!
+//! The following behavior is possible to implement in a [`Deserialize`] implementation, but is not
+//! supported within `serde_args`.
+//!
+//! - Not calling a method on the provided [`Deserializer`]
+//! - Calling `Deserializer::deserialize_struct()` on one run of `Deserialize::deserialize()`, and
+//!   then calling `Deserializer::deserialize_enum()` on the next run (or vice-versa).
+//! - Attempting to deserialize an identifier by calling anything besides
+//!   `Deserializer::deserialize_identifier()`.
+//! - Calling `Deserializer::deserialize_any()` or `Deserializer::deserialize_ignored_any()`.
+//!   `serde_args` is **not** a self-describing format.
+//!
+//! [`Deserialize`]: serde::Deserialize
+//! [`Deserializer`]: serde::Deserializer
+//! [`Deserializer::deserialize_unit()`]: serde::Deserializer::deserialize_unit()
+//! [`Deserializer::deserialize_unit_struct()`]: serde::Deserializer::deserialize_unit_struct()
+//! [`Deserializer::deserialize_bool()`]: serde::Deserializer::deserialize_bool()
+//! [`Deserializer::deserialize_u32()`]: serde::Deserializer::deserialize_u32()
+//! [`Deserializer::deserialize_option()`]: serde::Deserializer::deserialize_option()
+//! [`FromStr`]: std::str::FromStr
+//! [`VariantAccess::unit_variant()`]: serde::de::VariantAccess::unit_variant()
+//! [`Visitor::expecting()`]: serde::de::Visitor::expecting()
